4041245477ef1bf9563871f0d306f2affb7c2c47
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-11 19:48:17 -0500

    Add shell to app.js component, add CardList and CardListItem component

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,35 @@
 function App() {
-    return <div className="App"></div>;
+  return (
+    <div className="min-h-screen bg-black-100">
+      <div className="bg-white shadow-sm">
+        <div className="max-2-7x1 mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16">
+            <div className="flex">
+              <div className="flex-shrink-0 flex itsems-center">
+                <p>Flashcards</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="py-10">
+        <header>
+          <div className="max-2 7x1 mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3x1 font-bold leading-tight text-gray-900">
+              Your Collections
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <div className="px-4 py-8 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/cardList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardListItem from "./cardListItem";
+
+const cardList = ({ cards }) => (
+  <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    {cards.map((card) => (
+      <CardListItem title={card.name} subtitle={card.name} />
+    ))}
+  </div>
+);
+
+export default cardList;
