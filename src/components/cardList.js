9bd4ed9c8c09d7a609f919a5cfa47d4ea29e7fa9
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-14 12:12:52 -0400

    Update title of page on index.html file.

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CardListItem from "./cardListItem";
 
-const cardList = ({ cards }) => (
+const cardList = ({ cards = [] }) => (
   <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
     {cards.map((card) => (
       <CardListItem title={card.name} subtitle={card.name} />
